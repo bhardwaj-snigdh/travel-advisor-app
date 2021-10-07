4909c3e40b3933c4ca13832bfa476c33679d4e74
Snigdh Bhardwaj <59894983+bhardwaj-snigdh@users.noreply.github.com>
2021-10-07 13:25:26 +0530

    feat: implement list component and place filtering
    list all places of specified type, each place is displayed as a card in list, places can be filtered based on rating

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,14 @@ import Map from './components/Map';
 
 export default function App() {
   const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
 
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [type, setType] = useState('restaurants');
+  const [ratings, setRatings] = useState(0);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords }) => {
@@ -22,10 +25,17 @@ export default function App() {
 
   useEffect(() => {
     if (!bounds) return;
+    setIsLoading(true);
     getPlacesData(bounds.sw, bounds.ne, type).then((data) => {
       setPlaces(data);
+      setFilteredPlaces([]);
+      setIsLoading(false);
     });
   }, [bounds, type]);
+
+  useEffect(() => {
+    setFilteredPlaces(places.filter((place) => place.rating > ratings));
+  }, [ratings]);
 
   return (
     <>
@@ -38,7 +48,14 @@ export default function App() {
         style={{ width: '100%' }}
       >
         <Grid item xs={12} md={4}>
-          <List />
+          <List
+            places={filteredPlaces.length > 0 ? filteredPlaces : places}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            ratings={ratings}
+            setRatings={setRatings}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,64 @@
-export default function List() {
+import {
+  CircularProgress,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+
+import PlaceDetails from '../PlaceDetails';
+import useStyles from './styles';
+
+export default function List({
+  places,
+  isLoading,
+  type,
+  setType,
+  ratings,
+  setRatings,
+}) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>List component</h1>
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Ratings</InputLabel>
+            <Select
+              value={ratings}
+              onChange={(e) => setRatings(e.target.value)}
+            >
+              <MenuItem value={0}>All</MenuItem>
+              <MenuItem value={3}>Above 3.0</MenuItem>
+              <MenuItem value={4}>Above 4.0</MenuItem>
+              <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid item key={i} xs={12}>
+                <PlaceDetails place={place} />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
     </div>
   );
 }
